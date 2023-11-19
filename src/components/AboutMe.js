978331ef1aa5f1b74e41d7cb72886cf149361e0c
Kev1nJ <kevinjerome85@yahoo.com>
2023-11-18 21:49:41 -0500

    src compenets created, index html created needs lots of designing and adjusting, css capabilities needed, viteconfig created, structure needs reevaluation

---
+++ src/components/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      {/* photo/avatar and short bio here */}
+    </section>
+  );
+};
+
+export default AboutMe;
