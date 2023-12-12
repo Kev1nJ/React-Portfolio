73f877624f63a222cae5900ca7d9a820bd7a6be4
Kev1nJ <kevinjerome85@yahoo.com>
2023-12-12 16:59:36 -0500

    aboutme info added

--- src/components/UI/Portfolio.jsx
+++ src/components/UI/Portfolio.jsx
@@ -4,20 +4,12 @@ const Portfolio = () => {
   return (
     <section id="portfolio">
       <h2>Portfolio</h2>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+      Hello, I'm Kevin Jerome, aspiring software developer/engineer with a passion for crafting seamless digital experiences. 
+      somewhat proficient in the MERN stack, Ive been honing my skills in MongoDB, Express.js, React.js, and Node.js, ensuring dynamic and responsive applications. 
+      My knowledge extends to leveraging technologies like Bootstrap for polished UIs and navigating RESTful APIs effortlessly. 
+      Testing is a crucial part of my workflow, and I employ tools such as Postman and Insomnia to ensure robust functionality. 
+      With a desire to expand my toolkit, I am actively delving into Docker, Python, and AWS, driven by a relentless curiosity for cutting-edge technologies. 
+      As I continue on this journey, my sights are set on possibly exploring cybersecurity and cloud engineering, anticipating a future where innovation and security go hand in hand.
 
   </section>
   );
